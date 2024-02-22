a7b03e6d7aed11c318701f2cabe5caafb4f6a4bb
wtucker <wtbtucker@gmail.com>
2024-02-22 13:53:38 -0500

    Switch to import_module

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from importlib import util
+from importlib import import_module
 
-spec = util.spec_from_file_location('app', 'C:\\Users\\order\\repos\\pace-calculator\\applications\\data-collector\\main\\app.py')
-app = util.module_from_spec(spec)
-spec.loader.exec_module(app)
+app = import_module('applications.data-collector.main.app')
 
 if __name__ == "__main__":
     app.run()
