4084326a20e61e84edc3ec891f9e95d7b5dfc83a
wtucker <wtbtucker@gmail.com>
2024-02-26 07:29:29 -0500

    Move home page to separate templates folder

--- applications/web-app/main/app.py
+++ applications/web-app/main/app.py
@@ -1,20 +1,15 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_zip_code">
-         <input type="submit" value="Submit">
-         <label for="user_zip_code">Zip Code</label><br>
-     </form>
-     '''
+    return render_template('index.html')
 
-@app.route("/echo_user_input", methods=["POST"])
+@app.route("/forecast", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_zip_code", "")
     return "Your zip code: " + input_text
