8d19a041b5ba4d3dfa46794028e7e450d32b32b2
wtucker <wtbtucker@gmail.com>
2024-02-27 19:47:25 -0500

    Move initialization logic for data-collector back to its own folder. This allows the cron job to run on only data collector with a simple command

--- backend/DataCollector/main/__init__.py
+++ backend/DataCollector/main/__init__.py
@@ -1,3 +1,15 @@
 # For relative imports to work in Python 3.6
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from extensions import db
+from flask import Flask
+from DataCollectorRoutes import data_collector_bp
 
+def create_app(database_url='postgresql://postgres:postgres@localhost/weather'):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+    # with app.app_context():
+    #     db.create_all()
+
+    db.init_app(app)
+    app.register_blueprint(data_collector_bp)
+    return app
