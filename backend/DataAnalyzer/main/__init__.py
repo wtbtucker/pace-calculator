124bccfe9641d8b228769b2d1a6be51a18d56836
wtucker <wtbtucker@gmail.com>
2024-02-27 20:01:41 -0500

    Separate initialization logic for DataAnalyzer and DataCollector into separate init files for each app. Import the db module for the whole project into each create_app function

--- backend/DataAnalyzer/main/__init__.py
+++ backend/DataAnalyzer/main/__init__.py
@@ -1,1 +1,12 @@
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from extensions import db
+from flask import Flask
+from DataAnalyzerRoutes import data_analyzer_bp
+
+def create_app(database_url='postgresql://postgres:postgres@localhost/weather'):
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+
+    db.init_app(app)
+    app.register_blueprint(data_analyzer_bp)
+    return app
