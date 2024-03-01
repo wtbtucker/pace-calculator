ea66d8f2db5713f4121491213725469c7559cf7d
wtucker <wtbtucker@gmail.com>
2024-03-01 07:55:47 -0500

    Switch back to using init file for gunicorn

--- web-app/main/wsgi.py
+++ web-app/main/wsgi.py
@@ -1,5 +1,5 @@
-from . import create_app
+# from . import create_app
 
-application = create_app()
-if __name__ == "__main__":
-    application.run()
+# application = create_app()
+# if __name__ == "__main__":
+#     application.run()
