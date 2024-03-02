1949f26903c25bf4bd0e6fddf5766098d222cb2c
wtucker <wtbtucker@gmail.com>
2024-03-02 12:01:03 -0500

    Load rabbitmq url from environmental variables

--- backend/DataAnalyzer/main/Publisher.py
+++ backend/DataAnalyzer/main/Publisher.py
@@ -1,13 +1,34 @@
+import os
+from dotenv import load_dotenv
 import pika
+
+
+class Credentials:
+    def __init__(self, username: str=None, password: str=None, erlang_cookie: str=None):
+        self.USERNAME = username
+        self.PASSWORD = password
+        self.COOKIE = erlang_cookie
+    
+    def read_from_env(self):
+        load_dotenv()
+        self.USERNAME = os.getenv('RABBITMQ-USERNAME')
+        self.PASSWORD = os.getenv('RABBITMQ_PASSWORD')
+        self.COOKIE = os.getenv('RABBITMQ_ERLANG_COOKIE')
+
 
 class Publisher:
     def __init__(self):
-        # TCP internal address for broker hosted on Render
-        connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq-1wih:4369'))
+        # HTTP  internal address for broker hosted on Render
+        RABBITMQ_URL = self._load_url()
+        connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_URL))
         self.channel = connection.channel
-        self.channel.queue_declare(queue='zipcode')
+
+    def _load_url(self):
+        load_dotenv()
+        return os.getenv('RABBITMQ_URL')
     
-    def send_zipcode(self, zipcode):
+    def send_zipcode(self):
+        self.channel.queue_declare(queue='zipcode')
         self.channel.basic_publish(exchange='',
                       routing_key='zipcode',
                       body='02155')
