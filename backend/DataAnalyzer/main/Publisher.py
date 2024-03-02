3c6320b1fa18f1524a0c139c77b90c3682a9e860
wtucker <wtbtucker@gmail.com>
2024-03-02 12:09:08 -0500

    Remove dotenv import and usage

--- backend/DataAnalyzer/main/Publisher.py
+++ backend/DataAnalyzer/main/Publisher.py
@@ -1,31 +1,29 @@
-import os
-from dotenv import load_dotenv
 import pika
 
 
-class Credentials:
-    def __init__(self, username: str=None, password: str=None, erlang_cookie: str=None):
-        self.USERNAME = username
-        self.PASSWORD = password
-        self.COOKIE = erlang_cookie
+# class Credentials:
+#     def __init__(self, username: str=None, password: str=None, erlang_cookie: str=None):
+#         self.USERNAME = username
+#         self.PASSWORD = password
+#         self.COOKIE = erlang_cookie
     
-    def read_from_env(self):
-        load_dotenv()
-        self.USERNAME = os.getenv('RABBITMQ-USERNAME')
-        self.PASSWORD = os.getenv('RABBITMQ_PASSWORD')
-        self.COOKIE = os.getenv('RABBITMQ_ERLANG_COOKIE')
+#     def read_from_env(self):
+#         load_dotenv()
+#         self.USERNAME = os.getenv('RABBITMQ-USERNAME')
+#         self.PASSWORD = os.getenv('RABBITMQ_PASSWORD')
+#         self.COOKIE = os.getenv('RABBITMQ_ERLANG_COOKIE')
 
 
 class Publisher:
     def __init__(self):
         # HTTP  internal address for broker hosted on Render
         RABBITMQ_URL = self._load_url()
-        connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_URL))
+        connection = pika.BlockingConnection(pika.ConnectionParameters('amqp://rabbitmq:rabbitmq@rabbitmq-1wih:15672'))
         self.channel = connection.channel
 
-    def _load_url(self):
-        load_dotenv()
-        return os.getenv('RABBITMQ_URL')
+    # def _load_url(self):
+    #     load_dotenv()
+    #     return os.getenv('RABBITMQ_URL')
     
     def send_zipcode(self):
         self.channel.queue_declare(queue='zipcode')
